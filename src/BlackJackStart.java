b6a3572ab3cbb6d02975af7821c008ce05dc8569
AustinP11 <AustinParente11@gmail.com>
2017-01-12 17:51:11 -0500

    Update BlackJackStart.java

--- src/BlackJackStart.java
+++ src/BlackJackStart.java
@@ -2,13 +2,13 @@
  *Group
  *JAVA
  */
- 
- //awt's
- import java.awt.*;
- import java.awt.event.ActionListener;
- import java.awt.event.ActionEvent;
- //swing's
- import javax.swing.*;
+
+//awt's
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+//swing's
+import javax.swing.*;
 
 
 public class BlackJackStart extends JFrame implements ActionListener
@@ -28,96 +28,101 @@ public class BlackJackStart extends JFrame implements ActionListener
 	JComboBox players = new JComboBox(playersArr);
 
 	Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 28);
-	
+
 	//Button
 	JButton pressMe = new JButton("Start");
-	
-		int bet1 = 0;
- 	int bet2 = 0;
+
+	int bet1 = 0;
+	int bet2 = 0;
 	int bet3 = 0;
- 	int bet4 = 0;
- 	
+	int bet4 = 0;
+
 	int totalCoin1 = 100;
 	int totalCoin2 = 100;
 	int totalCoin3 = 100;
-	int totalCoin4 = 100;	
-	
- 	//JLabel [] betting = {"Place Your Bets!"};
-	String [] betValue = {"5","10","20","30","40","50","All In"}; 
+	int totalCoin4 = 100;
+
+	//JLabel [] betting = {"Place Your Bets!"};
+	String [] betValue = {"5","10","20","30","40","50","All In"};
 	//combos should not be able to select a value untill the num players is selected.
 	JComboBox player1 = new JComboBox(betValue);
 	JComboBox player2 = new JComboBox(betValue);
 	JComboBox player3 = new JComboBox(betValue);
 	JComboBox player4 = new JComboBox(betValue);
-	
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	public BlackJackStart()
 	{
-	
+
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setSize(450,450);
 		setVisible(true);
 		setLocationRelativeTo(null);
-		
-	player1.setSelectedIndex(0);
-	player1.addActionListener(this);
-	
-	player2.setSelectedIndex(0);
-	player2.addActionListener(this);
-	
-	player3.setSelectedIndex(0);
-	player3.addActionListener(this);
-	
-	player4.setSelectedIndex(0);
-	player4.addActionListener(this);
-	
+
+		player1.setSelectedIndex(0);
+		player1.addActionListener(this);
+
+		player2.setSelectedIndex(0);
+		player2.addActionListener(this);
+
+		player3.setSelectedIndex(0);
+		player3.addActionListener(this);
+
+		player4.setSelectedIndex(0);
+		player4.addActionListener(this);
+
 
 		pressMe.addActionListener(this);
 		players.addActionListener(this);
-		
+
 		northLbl.setFont(font);
 		pressMe.setFont(font);
 		players.setFont(font);
-		
+
 		//main panel
 		add(mainPnl);
 		mainPnl.setLayout(border);
-		
+
 		// north panel
 		northPnl.add(northLbl);
 		mainPnl.add(northPnl, BorderLayout.NORTH);
-		
+
 		//southPnl.add(southLbl);
 		southPnl.add(pressMe);
 		mainPnl.add(southPnl, BorderLayout.SOUTH);
-		
+
 		//east panel
 		mainPnl.add(eastPnl, BorderLayout.EAST);
-		
+
 		//west panel
+		westPnl.setLayout(new BoxLayout(westPnl, BoxLayout.Y_AXIS));
+		westPnl.add(player1);
+		westPnl.add(player2);
+		westPnl.add(player3);
+		westPnl.add(player4);
 		mainPnl.add(westPnl, BorderLayout.WEST);
-		
+
 		//center panel
 		centerPnl.add(players);
 		mainPnl.add(centerPnl, BorderLayout.CENTER);
-		
+
 		//mainPnl.add(betting);
 		// it should say betting then under it should have the 4 drop downs
-		mainPnl.add(player1);
-		mainPnl.add(player2);
-		mainPnl.add(player3);
-		mainPnl.add(player4);
-	
-		
+		//mainPnl.add(player1);
+		//mainPnl.add(player2);
+		//mainPnl.add(player3);
+		//mainPnl.add(player4);
+
+
 	}
-	
-	
+
+
 	public void actionPerformed(ActionEvent e)
 	{
 		Object source = e.getSource();
@@ -126,7 +131,7 @@ public class BlackJackStart extends JFrame implements ActionListener
 			dispose();
 			Blackjack app = new Blackjack((String)players.getSelectedItem());
 
-			
+
 		}
 		if(source == player1)
 		{
@@ -138,7 +143,7 @@ public class BlackJackStart extends JFrame implements ActionListener
 					break;
 				case "10" : bet1 = 10;
 					break;
-				case "20" : bet1 = 20;					
+				case "20" : bet1 = 20;
 					break;
 				case "30" : bet1 = 30;
 					break;
@@ -150,7 +155,7 @@ public class BlackJackStart extends JFrame implements ActionListener
 					break;
 				default: bet1 = 5;
 			}
-		}		
+		}
 		if(e.getSource() == player2)
 		{
 			JComboBox bc = (JComboBox)e.getSource();
@@ -173,7 +178,7 @@ public class BlackJackStart extends JFrame implements ActionListener
 					break;
 				default: bet2 = 5;
 			}
-		}		
+		}
 		if(e.getSource() == player3)
 		{
 			JComboBox cd = (JComboBox)e.getSource();
@@ -196,7 +201,7 @@ public class BlackJackStart extends JFrame implements ActionListener
 					break;
 				default: bet3 = 5;
 			}
-		}			
+		}
 		if(e.getSource() == player4)
 		{
 			JComboBox de = (JComboBox)e.getSource();
@@ -219,12 +224,12 @@ public class BlackJackStart extends JFrame implements ActionListener
 					break;
 				default: bet4 = 5;
 			}
-		}		
-	
+		}
+
 	}
-	
+
 	public static void main(String [] args)
 	{
-		BlackJackStart myFrame = new BlackJackStart();	
-	}//end main method	
-}//end class JFramePractice 2 
+		BlackJackStart myFrame = new BlackJackStart();
+	}//end main method
+}//end class JFramePractice 2
